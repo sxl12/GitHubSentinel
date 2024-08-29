38b8467c2be10b6d41909983664220f4b7824da3
sxl12 <sxl_122@yeah.net>
2024-08-29 16:24:09 +0800

    add System role

--- src/llm.py
+++ src/llm.py
@@ -6,12 +6,18 @@ class LLM:
     def __init__(self):
         # 创建一个OpenAI客户端实例
         self.client = OpenAI()
+        self.client =OpenAI(
+            base_url="https://api.moonshot.cn/v1",
+            api_key=os.environ.get("MOONSHOT_API_KEY")
+        )
+        self.model = "moonshot-v1-32k"
         # 配置日志文件，当文件大小达到1MB时自动轮转，日志级别为DEBUG
         LOG.add("daily_progress/llm_logs.log", rotation="1 MB", level="DEBUG")
 
     def generate_daily_report(self, markdown_content, dry_run=False):
         # 构建一个用于生成报告的提示文本，要求生成的报告包含新增功能、主要改进和问题修复
-        prompt = f"以下是项目的最新进展，根据功能合并同类项，形成一份简报，至少包含：1）新增功能；2）主要改进；3）修复问题；:\n\n{markdown_content}"
+        prompt = markdown_content
+        system_prompt ="你是专业的项目经理，擅长项目进度分析，以下是项目最新进展，分析后生成中文报告，包含新增功能,主要改动,修复问题"
         
         if dry_run:
             # 如果启用了dry_run模式，将不会调用模型，而是将提示信息保存到文件中
@@ -27,8 +33,9 @@ class LLM:
         try:
             # 调用OpenAI GPT模型生成报告
             response = self.client.chat.completions.create(
-                model="gpt-3.5-turbo",  # 指定使用的模型版本
+                model=self.model,  # 指定使用的模型版本
                 messages=[
+                    {"role": "system", "content": system_prompt},
                     {"role": "user", "content": prompt}  # 提交用户角色的消息
                 ]
             )
